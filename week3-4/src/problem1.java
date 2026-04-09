f4954038f346a4f7d808ba640b194c1698843a25
prajitgunasekaran-droid <prajit.gunasekaran@gmail.com>
2026-04-09 08:52:27 +0530

    week3-4 problem1

---
+++ week3-4/src/problem1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Transaction {   // 👈 REQUIRED
+    String id;
+    double fee;
+    String timestamp;
+
+    Transaction(String id, double fee, String timestamp) {
+        this.id = id;
+        this.fee = fee;
+        this.timestamp = timestamp;
+    }
+
+    public String toString() {
+        return id + ":" + fee + "@" + timestamp;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        transactions.add(new Transaction("id1", 10.5, "10:00"));
+        transactions.add(new Transaction("id2", 25.0, "09:30"));
+        transactions.add(new Transaction("id3", 5.0, "10:15"));
+
+        System.out.println(transactions);
+    }
+}
